f4695ceba6447ee185851abc313e71614f83e2f9
dbochoa77 <dbochoa77@proton.me>
2025-06-08 08:37:14 -0700

    Added my Nix Configuration Dependencies

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,16 @@
+#################################
+# Services Needed to be Enabled #
+#################################
+  services.pcscd.enable = true;
 
+###################
+# Packages Needed #
+###################
+ environment.systemPackages = with pkgs; [
+# Cac Card Tools
+  pcsclite
+  pcsc-tools
+  opensc
+  ccid
+  nssTools
+];
